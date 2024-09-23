8b49db29e7963fe4e74d9d17d0717dc1e1c2c518
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-09-23 22:53:07 +0500

    Task1

---
+++ Tyuiu.OsadetsAA.Sprint1.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.OsadetsAA.Sprint1.Task1.V25.Lib;
+namespace Tyuiu.OsadetsAA.Sprint1.Task1.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
