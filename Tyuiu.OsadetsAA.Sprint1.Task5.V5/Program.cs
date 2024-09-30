f263f03d9f3c29415626fc7d06ac63af8bfaf82d
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-09-30 22:11:09 +0500

    Task5

--- Tyuiu.OsadetsAA.Sprint1.Task5.V5/Program.cs
+++ Tyuiu.OsadetsAA.Sprint1.Task5.V5/Program.cs
@@ -7,17 +7,18 @@ namespace Tyuiu.OsadetsAA.Sprint1.Task5.V5
         {
             DataService ds = new DataService();
 
-            Console.Title = ("Спринт #0 | Выполнил: Осадец А. А. | ПКТб-24-1");
+            Console.Title = ("Спринт #1 | Выполнил: Осадец А. А. | ПКТб-24-1");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
-            Console.WriteLine("* Задание #7                                                              *");
-            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
             Console.WriteLine("* Выполнил: Осадец Александр Александрович | ПКТб-24-1                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
-            Console.WriteLine("* одинаковых массивов по длинне.                                          *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части              *");
+            Console.WriteLine("* положительного вещественного числа x (так, если x=32.597, то d=5).      *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
