33efc28a299caa1664b40e126e2654e0aa1dbf00
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-09-30 22:09:38 +0500

    Task5

---
+++ Tyuiu.OsadetsAA.Sprint1.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.OsadetsAA.Sprint1.Task5.V5.Lib
+{
+    public class DataService : ISprint1Task5V5
+    {
+        public int Calculate(double x)
+        {
+            double FractionalPart = x - Math.Truncate(x);
+            int FirstDigit = (int)(FractionalPart * 10);
+            return FirstDigit;
+        }
+    }
+}
---
+++ Tyuiu.OsadetsAA.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.OsadetsAA.Sprint1.Task5.V5.Lib;
+namespace Tyuiu.OsadetsAA.Sprint1.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = ("Спринт #0 | Выполнил: Осадец А. А. | ПКТб-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Осадец Александр Александрович | ПКТб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+            Console.WriteLine("* одинаковых массивов по длинне.                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+        }
+    }
+}
